95cfae9c7af78019d40b342c1df076fd0be5bfd1
Abhay <abhaysharma.developer@gmail.com>
2024-09-01 03:12:30 +0530

    Initial commit

---
+++ apps/user-app/app/page.tsx
@@ -0,0 +1,9 @@
+import { PrismaClient } from "@repo/db/client";
+
+export default function Home() {
+  return (
+    <main className="bg-slate-400 w-screen h-screen flex justify-center items-center">
+      Hello
+    </main>
+  );
+}
