fdae7c4c7aafff22e59705592b83903e052db7ca
Abhay <abhaysharma.developer@gmail.com>
2024-09-03 23:37:13 +0530

    Bank webhook server added

---
+++ apps/bank-webhook/src/index.ts
@@ -0,0 +1,42 @@
+import express from "express";
+import db from "@repo/db/client";
+const app = express();
+
+app.post("/hdfcWebhook", async (req, res) => {
+  //TODO: Add zod validation here?
+  //TODO : Check if this request came from HDFC bank, use a webhook secret here
+
+  const paymentInformation = {
+    token: req.body.token,
+    userId: req.body.user_identifier,
+    amount: req.body.amount,
+  };
+  await db.balance.update({
+    where: {
+      userId: paymentInformation.userId,
+    },
+    data: {
+      amount: {
+        increment: paymentInformation.amount,
+      },
+    },
+  });
+
+  await db.onRampTransaction.update({
+    where: {
+      token: paymentInformation.token,
+    },
+    data: {
+      status: "Success",
+    },
+  });
+
+  res.json({
+    message: "captured",
+  });
+  // Update balance in db, add txn
+});
+
+app.listen(8080, () => {
+  console.log("abhay is a warrior");
+});
